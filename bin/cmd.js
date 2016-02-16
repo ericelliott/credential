6d028b851de7ed454ba01c26ae69a3a5bb3ca209
Thomas Sileghem <tsileghem@neworbit.co.uk>
2016-02-16 19:22:05 +0000

    chore(eslint): update eslint

--- bin/cmd.js
+++ bin/cmd.js
@@ -8,51 +8,51 @@ var credential = require('../credential');
 var stdin = '';
 
 program
-	.command('hash [password]')
-	.description('Hash password')
-	.option('-w --work <work>', 'relative work load (0.5 for half the work)', Number)
-	.option('-k --key-length <key-length>', 'length of salt', Number)
-	.action(function (password, options){
-		var pw = credential(pluck([
-			'keyLength',
-			'hashMethod',
-			'work'
-		], options));
+  .command('hash [password]')
+  .description('Hash password')
+  .option('-w --work <work>', 'relative work load (0.5 for half the work)', Number)
+  .option('-k --key-length <key-length>', 'length of salt', Number)
+  .action(function (password, options){
+    var pw = credential(pluck([
+      'keyLength',
+      'hashMethod',
+      'work'
+    ], options));
 
-		pw.hash(stdin || password, function (err, result){
-			if (err){
-				return console.error(err);
-			}
+    pw.hash(stdin || password, function (err, result){
+      if (err){
+        return console.error(err);
+      }
 
-			console.log(result);
-		});
-	});
+      console.log(result);
+    });
+  });
 
 program
-	.command('verify [hash] <password>')
-	.description('Verify password')
-	.action(function (hash, password){
-		credential().verify(stdin || hash, password, function (err, valid){
-			if (err){
-				return console.error(err);
-			}
+  .command('verify [hash] <password>')
+  .description('Verify password')
+  .action(function (hash, password){
+    credential().verify(stdin || hash, password, function (err, valid){
+      if (err){
+        return console.error(err);
+      }
 
-			if (!valid){
-				throw new Error('Invalid');
-			}
+      if (!valid){
+        throw new Error('Invalid');
+      }
 
-			console.log('Verified');
-		});
-	});
+      console.log('Verified');
+    });
+  });
 
 if (process.stdin.isTTY) {
-	program.parse(process.argv);
+  program.parse(process.argv);
 } else {
-	process.stdin.on('readable', function (){
-		stdin += this.read() || '';
-	});
+  process.stdin.on('readable', function (){
+    stdin += this.read() || '';
+  });
 
-	process.stdin.on('end', function (){
-		program.parse(process.argv);
-	});
+  process.stdin.on('end', function (){
+    program.parse(process.argv);
+  });
 }
